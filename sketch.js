e356f54a424292d12f6401c245080fe918226a1b
Glitch (car-game) <none>
2020-07-17 01:31:03 +0000

    Updated with Glitch

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
-
+let redCar
+let road
 function setup() {
-createCanvas(1600,800)
+  createCanvas(1600,800)
+  redCar = loadImage('https://cdn.glitch.com/b92fac6a-290b-43ed-b913-a4ce6c1aeedf%2Fred_car.png?v=1594948385920')
+  road = loadImage('https://cdn.glitch.com/b92fac6a-290b-43ed-b913-a4ce6c1aeedf%2Froad.png?v=1594948392204')
 
 }
 
 function draw() {
 	background(220)
-    fill(0)
-    line(0,300,width,300)
-    line(0,500,width,500)
+  image(road,0,400-road.width/4)
+  image(redCar,800,400)
 }
